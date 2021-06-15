d0c715a1a135acdde4d9c560a4ea22e31d18cff3
Jose Ledezma Cortes <jrledezma@users.noreply.github.com>
2021-06-15 14:53:11 -0600

    cambio de puerto

--- server.ts
+++ server.ts
@@ -2,12 +2,12 @@ import app from './src/api/app';
 import { CommonFunctions } from './src/api/common/commonFunctions';
 import { PrintColorType } from './src/api/enums/printColorType.enum';
 
-const port = 8080;
-//const port = process.env.PORT || 8080;
+//const port = 8080;
+const port = process.env.PORT || 8080;
 
 let listenServer = app.listen(port, (): void => {
   CommonFunctions.PrintConsoleColor(
-    'shufeng jiedu  webapp-backend listening on port ' + port.toString(),
+    'escudo-antigripal-webapp-server is listening on port ' + port.toString(),
     PrintColorType.info
   );
 });
